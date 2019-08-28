3df33b422fc8897e0e0e57bd57a0e4e63b63d4a4
Luke Hansen <luhansen@microsoft.com>
2019-08-28 14:40:26 -0700

    added grammar

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -6,9 +6,16 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
+            grammar();
             Console.WriteLine("Hello World!");
             int x = 1;
             Console.WriteLine(x + x + x + x + x - x - x - x - x - x - x - x - x - x - x - x - x + x * 2 + x - 1 * (-4 + 3));
+        }
+
+        static void grammar()
+        {
+            //Grammar
+            Console.WriteLine("I is a code writer and being good with the words of grammars.");
         }
     }
 }
