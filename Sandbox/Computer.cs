eecd73c4dda696c3dc3b9796036c7347505dd868
Luke Hansen <luhansen@microsoft.com>
2019-08-28 14:47:52 -0700

    added philosophical inquiry

---
+++ Sandbox/Computer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sandbox
+{
+    class Computer
+    {
+        //WHAT IF COMPUTERS WERE JUST BIG CALCULATORS?!?!??!!??!
+    }
+}
