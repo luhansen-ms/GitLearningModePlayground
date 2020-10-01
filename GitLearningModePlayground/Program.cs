85bfbc7cbe6e468c0c573792c8c7a474b6bef955
Luke Hansen <luhansen@microsoft.com>
2020-10-01 16:12:32 -0700

    asdf

--- GitLearningModePlayground/Program.cs
+++ GitLearningModePlayground/Program.cs
@@ -8,7 +8,7 @@ namespace GitLearningModePlayground
         {
             Console.WriteLine("Welcome to the Git Learning Mode Playground!");
             Console.WriteLine("Feel free to use this repository to play and experiment with Git in Visual Studio.");
-            // teat
+            // test!
         }
     }
 }
